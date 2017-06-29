14db9cdc7efd0959959d2f4b7b266b56288ee797
Nicklaus McClendon <nicklaus@kulinacs.com>
2017-06-28 22:06:56 -0500

    Fixed Kiosk update script

--- roles/kiosk/files/update-scroller.sh
+++ roles/kiosk/files/update-scroller.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 if [ -d "/mnt/council_scroller/scroller" ]; then
     rm /opt/announcements/*
-    rsync /mnt/council_scroller/scroller/ /opt/announcements/
+    rsync /mnt/council_scroller/scroller/* /opt/announcements/
 fi
